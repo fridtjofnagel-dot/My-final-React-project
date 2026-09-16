3ac82b4dd84c816611f934cc98fe576aea1c7ff2
fridtjofnagel-dot <fridtjofnagel@gmail.com>
2026-09-16 18:40:25 +0200

    feat: AppState and AppContext finalized to make it useable in different components

---
+++ src/context/AppState.jsx
@@ -0,0 +1,25 @@
+import { AppContext } from '.'
+import { useState } from 'react'
+import { getNotes } from '../storage/noteStorage'
+import { getPokemon } from '../storage/pokemonStorage'
+ 
+
+function AppState () {
+    const [pokemon, setPokemon] = useState([])
+    const [notes, setNotes] = useState([])
+
+    return (
+        <AppContext.Provider
+            value={{
+                pokemon,
+                setPokemon,
+                notes,
+                setNotes
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export default AppState
