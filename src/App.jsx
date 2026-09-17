9994af41a762284425b734a391884a90c4d2927a
fridtjofnagel-dot <fridtjofnagel@gmail.com>
2026-09-17 12:02:49 +0200

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { AppState, AppContext } from './context'
+import { MainLayout } from './layouts/MainLayout'
+import { CoverLayout } from './layouts/CoverLayout'
+import { Browserrouter, Router, Routes, Route } from 'react-router-dom'
+ 
+
 
 function App() {
   return (
     <div>
-
-  </div>
+      <AppState>
+        <Routes>
+          <Route>
+            <Route path='./'
+          </Route>
+        </Routes>
+      </AppState>
+    </div>
   )
   }
 
